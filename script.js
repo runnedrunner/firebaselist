930cad17ced5329444f4d084b97598913c9174cb
runnedrunner <auszhou@nuevaschool.org>
2016-10-05 08:35:01 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-var input = $("")
+var input = $("#new-item-input")
+var clearButton = $("#clear-button")
 
 input.keydown(function(e) {	
   if (e.keyCode == "13") {
     var valueToSave = input.val();
-    addListItem(valueToSave);
+    addListItem("davidsList", valueToSave);
   }
+})
+
+clearButton.click(function(e) {	
+  deleteValue("davidsList")
 })
 
 onNewListItem("davidsList", function(value) {
